bcbdee7416a5ccd7ec6542f70dcfeb707bdbd823
OICHAN <to2319work@gmail.com>
2025-03-22 15:05:34 +0900

    202503221505

--- App_final.py
+++ App_final.py
@@ -124,7 +124,7 @@ st.markdown("""
 
 """, unsafe_allow_html=True)
 
-st.markdown('<div class="big-title">東京ガスの機器交換<br>スキルアップクイズ</div>', unsafe_allow_html=True)
+st.markdown('<div class="big-title">〇〇ガスの機器交換<br>スキルアップクイズ</div>', unsafe_allow_html=True)
 
 # 初期化
 if "started" not in st.session_state:
